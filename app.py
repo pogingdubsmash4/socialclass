2222feab90e04f98da3053619d9d87c7603e40cb
pogingdubsmash4 <lorenzo.eser27@gmail.com>
2025-05-03 11:29:38 +0800

    Your descriptive message here

--- app.py
+++ app.py
@@ -161,8 +161,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-# --- START APP ---
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+@app.before_first_request
+def create_tables():
+    db.create_all()
